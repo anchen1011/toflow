05f3ee78958dd4ed8b7ddf54f12b6b10a2a6180a
Baian Chen (Andrew) <anchen1011@gmail.com>
2017-11-01 23:29:07 -0400

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,8 +1,8 @@
 cd data
 wget http://data.csail.mit.edu/tofu/dataset/vimeo_tri.zip
-unzip vimeo_tri.zip
+unzip vimeo_tri.zip -d vimeo_tri
 rm vimeo_tri.zip
 wget http://data.csail.mit.edu/tofu/dataset/vimeo_sep.zip
-unzip vimeo_sep.zip
+unzip vimeo_sep.zip -d vimeo_sep
 rm vimeo_sep.zip
 cd ..
