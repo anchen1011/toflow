0c74437480be6c03cd5e08863b3d7fe218fd95b2
Tianfan Xue <jimmyxuetianfan@gmail.com>
2018-05-16 09:28:56 -0700

    Fix a bug in get_task.m when taskname == 'interp'

--- src/evaluation/get_task.m
+++ src/evaluation/get_task.m
@@ -9,7 +9,7 @@ tasklist{1,4} = output_dir;                         % Output dir
 tasklist{1,5} = 'sep';                              % Subpaths class (corresponding to the suffix after get_path)
 tasklist{1,6} = 'im4.png';                          % The name of target file under the target path
 tasklist{1,7} = 'out.png';                          % The name of output file under the target path
-if task == 'interp'
+if strcmp(task, 'interp')
   tasklist{1,2} = 3782;
   tasklist{1,5} = 'tri';
   tasklist{1,6} = 'im2.png';
