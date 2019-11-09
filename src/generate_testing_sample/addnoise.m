11ca7bd3175d119068b3909fb332f6c3e2460b9e
Baian Chen (Andrew) <anchen1011@gmail.com>
2019-11-09 16:11:12 -0500

    Update addnoise.m
     Gaussian noise instead of half Gaussian

--- src/generate_testing_sample/addnoise.m
+++ src/generate_testing_sample/addnoise.m
@@ -2,7 +2,8 @@ function y = addnoise(I)
 
 h = size(I,1);
 w = size(I,2);
-y = uint8(uint8(I) + uint8(random('norm',0,25.5,h,w,3)));
+y = uint8(zeros(h, w, 3));
+I = uint8(int16(I) + int16(random('norm',0,25.5,h,w,3)));
 b = uint8(rand(h, w ,3) * 255);
 tm = rand(h, w) > 0.1;
 m = zeros(h, w, 3);
@@ -10,4 +11,4 @@ m(:,:,1) = tm;
 m(:,:,2) = tm;
 m(:,:,3) = tm;
 rm = 1 - m;
-y = (y .* uint8(m)) + (b .* uint8(rm));
+y = (I .* uint8(m)) + (b .* uint8(rm));
